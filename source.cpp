a252e9f50b20a6f6d245a6b432eef562a8f28191
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-11-21 15:23:44 +0100

    Refactor and repair multiply method

--- source.cpp
+++ source.cpp
@@ -38,228 +38,233 @@ class FailureStreamException: public exception
         }    
 };
 
-class Matrix 
+
+Matrix::Matrix()
 {
-    private:
-        int colsSize;
-        int rowsSize;
-        vector<vector<double>> matrix;
+    int const deafaultValue = 2;
+    this->colsSize = deafaultValue;
+    this->rowsSize = deafaultValue; 
+    initMatrix(deafaultValue, deafaultValue);
+}
 
-    public:
+Matrix::Matrix(int rowsSize, int colsSize)
+{
+    if(rowsSize <= 0 || colsSize <= 0)
+    {
+        throw IncorrectSizeException();
+    }
+    this->colsSize = colsSize;
+    this->rowsSize = rowsSize; 
+    initMatrix(rowsSize, colsSize);
+}
 
-        Matrix()
-        {
-            int const deafaultValue = 2;
-            this->colsSize = deafaultValue;
-            this->rowsSize = deafaultValue; 
-            initMatrix(deafaultValue, deafaultValue);
+Matrix::Matrix(int size)
+{
+    if(size <= 0)
+    {
+        throw IncorrectSizeException();
+    }
+    this->colsSize = size;
+    this->rowsSize = size; 
+    initMatrix(size, size);
+}
+
+Matrix::Matrix(string filename, string path){
+    ifstream readFile(path + '/' + filename);
+    if(!readFile.is_open()){
+        throw FailureOpenFileException();
+    }
+
+    readFile >> this->colsSize >> this->rowsSize;
+    initMatrix(this->rowsSize, this->colsSize);
+
+    readMatrixFromFile(readFile);
+    readFile.close();
+}
+
+Matrix::Matrix(const Matrix &matrixToCopy)
+{
+    this->colsSize = matrixToCopy.colsSize;
+    this->rowsSize = matrixToCopy.rowsSize; 
+    initMatrix(rowsSize, colsSize);
+    makeCopyMatrix(matrixToCopy);
+}
+
+Matrix Matrix::add(Matrix m2)
+{
+    if(isTheSameSize(m2) == false){
+        throw IncorrectSizeException();
+    }
+    Matrix newMatrix(rowsSize, colsSize);
+    for(int indexR = 0; indexR < matrix.size(); indexR++) {
+        for(int indexC = 0; indexC < matrix[indexR].size(); indexC++){
+            double value = m2.get(indexR, indexC) + this->get(indexR, indexC);
+            newMatrix.set(indexR, indexC, value);
         }
+    }
+    return newMatrix;
+}
 
-        Matrix(int rowsSize, int colsSize)
+Matrix Matrix::subtract(Matrix m2)
+{
+    if(isTheSameSize(m2) == false){
+        throw IncorrectSizeException();
+    }
+    Matrix newMatrix(rowsSize, colsSize);
+    for(int indexR = 0; indexR < matrix.size(); indexR++) {
+        for(int indexC = 0; indexC < matrix[indexR].size(); indexC++){
+            double value = this->get(indexR, indexC) - m2.get(indexR, indexC);
+            newMatrix.set(indexR, indexC, value);
+        }
+    }
+    return newMatrix;
+}
+
+Matrix Matrix::multiply(Matrix m2)
+{
+    if(colsSize != m2.rowsSize){
+        throw IncorrectSizeException();
+    }
+
+    Matrix newMatrix(this->rowsSize, m2.colsSize);
+
+    for (int indexR = 0; indexR < this->rowsSize; indexR++)
+    { 
+        for (int indexC = 0; indexC < m2.colsSize; indexC++)
         {
-            if(rowsSize <= 0 || colsSize <= 0)
+            for (int k = 0; k < this->colsSize; k++)
             {
-                throw IncorrectSizeException();
-            }
-            this->colsSize = colsSize;
-            this->rowsSize = rowsSize; 
-            initMatrix(rowsSize, colsSize);
-        }
-
-        Matrix(int size)
-        {
-            if(size <= 0)
-            {
-                throw IncorrectSizeException();
-            }
-            this->colsSize = size;
-            this->rowsSize = size; 
-            initMatrix(size, size);
-        }
-
-        Matrix(string filename, string path){
-            ifstream readFile(path + '/' + filename);
-            if(!readFile.is_open()){
-                throw FailureOpenFileException();
-            }
-
-            readFile >> this->colsSize >> this->rowsSize;
-            initMatrix(this->rowsSize, this->colsSize);
-
-            readMatrixFromFile(readFile);
-            readFile.close();
-        }
-
-        Matrix(const Matrix &matrixToCopy)
-        {
-            this->colsSize = matrixToCopy.colsSize;
-            this->rowsSize = matrixToCopy.rowsSize; 
-            initMatrix(rowsSize, colsSize);
-            makeCopyMatrix(matrixToCopy);
-        }
-
-        Matrix add(Matrix m2)
-        {
-            if(isTheSameSize(m2) == false){
-                throw IncorrectSizeException();
-            }
-            Matrix newMatrix(rowsSize, colsSize);
-            for(int indexR = 0; indexR < matrix.size(); indexR++) {
-                for(int indexC = 0; indexC < matrix[indexR].size(); indexC++){
-                    double value = m2.get(indexR, indexC) + this->get(indexR, indexC);
-                    newMatrix.set(indexR, indexC, value);
-                }
-            }
-            return newMatrix;
-        }
-
-        Matrix subtract(Matrix m2)
-        {
-            if(isTheSameSize(m2) == false){
-                throw IncorrectSizeException();
-            }
-            Matrix newMatrix(rowsSize, colsSize);
-            for(int indexR = 0; indexR < matrix.size(); indexR++) {
-                for(int indexC = 0; indexC < matrix[indexR].size(); indexC++){
-                    double value = this->get(indexR, indexC) - m2.get(indexR, indexC);
-                    newMatrix.set(indexR, indexC, value);
-                }
-            }
-            return newMatrix;
-        }
-
-        Matrix multiply(Matrix m2)
-        {
-            if(colsSize != m2.rowsSize){
-                throw IncorrectSizeException();
-            }
-
-            Matrix newMatrix(rowsSize, m2.colsSize);
-
-            for (int indexR = 0; indexR < matrix.size(); indexR++)
-            { 
-                for (int indexC = 0; indexC < matrix[indexR].size(); indexC++)
-                {
-                    for (int k = 0; k < colsSize; k++)
-                    {
-                        double value = 
-                            (matrix[indexR][k] * m2.get(k, indexC)) + newMatrix.get(indexR, indexC);
-                        newMatrix.set(indexR, indexC, value);
-                    }
-                }
-            }
-            return newMatrix;
-        }
-
-        void store(string filename, string path)
-        {
-            ofstream outputFile(path + '/' + filename);
-            if(outputFile.fail()){
-                throw FailureOpenFileException();
-            }
-            outputFile<< this->colsSize << " " << this->rowsSize << endl;
-            printToFile(outputFile);
-            outputFile.close();
-        }
-
-        void set(int rowsPosition, int colsPosition, double val) 
-        {
-            if(isIndexOutOfBounds(rowsPosition, colsPosition)){
-                   throw IndexOutOfBoundsException();
-               }
-            
-            matrix[rowsPosition][colsPosition] = val;
-        }
-
-        double get(int rowsPosition, int colsPosition) const 
-        {
-            if(isIndexOutOfBounds(rowsPosition, colsPosition)){
-                   throw IndexOutOfBoundsException();
-               }
-
-            return matrix[rowsPosition][colsPosition];
-        }
-
-        int cols()
-        {
-            return this->colsSize;
-        }
-
-        int rows()
-        {
-            return this->rowsSize;
-        }
-
-        void print()
-        {
-           for (int i = 0; i < matrix.size(); i++) { 
-                for (int j = 0; j < matrix[i].size(); j++) 
-                    cout << setprecision(2) << fixed << matrix[i][j] << " "; 
-                cout << endl; 
-            }  
-        }
-    private:
-        void printToFile(ofstream &outputStream)
-        {
-            for (int i = 0; i < matrix.size(); i++) { 
-                for (int j = 0; j < matrix[i].size(); j++) 
-                    outputStream << matrix[i][j] << " "; 
-                outputStream << endl; 
-            }  
-        }
-
-        void initMatrix(int rowsSize, int colsSize)
-        {
-            double initValue = 0.0;
-            for(int indexR = 0; indexR < rowsSize;indexR++) {
-                vector<double> singleRow;
-                for(int indexC = 0; indexC < colsSize ;indexC++) {
-                    singleRow.push_back(initValue); // change to 0.0 for prod version
-                }
-                matrix.push_back(singleRow);
+                double value = 
+                    (matrix[indexR][k] * m2.get(k, indexC)) + newMatrix.get(indexR, indexC);
+                newMatrix.set(indexR, indexC, value);
             }
         }
+    }
+    return newMatrix;
+}
 
-        void readMatrixFromFile(std::ifstream &inputStream){
-            if(!inputStream.is_open()){
-                throw FailureStreamException();
-            }
-            double singlePosition;
-            for (int indexR = 0; indexR < this->rowsSize; indexR++){
-                for (int indexC = 0; indexC < this->colsSize; indexC++){
-                    inputStream >> singlePosition;
-                    this->set(indexR, indexC, singlePosition);
-                }      
-            }
+void Matrix::store(string filename, string path)
+{
+    ofstream outputFile(path + '/' + filename);
+    if(outputFile.fail()){
+        throw FailureOpenFileException();
+    }
+    outputFile<< this->colsSize << " " << this->rowsSize << endl;
+    printToFile(outputFile);
+    outputFile.close();
+}
+
+void Matrix::set(int rowsPosition, int colsPosition, double val) 
+{
+    if(isIndexOutOfBounds(rowsPosition, colsPosition)){
+            throw IndexOutOfBoundsException();
+        }
+    
+    matrix[rowsPosition][colsPosition] = val;
+}
+
+double Matrix::get(int rowsPosition, int colsPosition) const 
+{
+    if(isIndexOutOfBounds(rowsPosition, colsPosition)){
+            throw IndexOutOfBoundsException();
         }
 
-        void makeCopyMatrix(Matrix const &matrixToCopy){
-            double singlePosition;
-            for (int indexR = 0; indexR < this->rowsSize; indexR++){
-                for (int indexC = 0; indexC < this->colsSize; indexC++){
-                    double value = matrixToCopy.get(indexR, indexC);
-                    this->set(indexR, indexC, value);
-                }      
-            }
-        }
+    return matrix[rowsPosition][colsPosition];
+}
 
-        bool isTheSameSize(Matrix m2){
-            if(this->colsSize == m2.colsSize && this->rowsSize == m2.rowsSize){
-                return true;
-            }
-            return false;
-        }
+int Matrix::cols()
+{
+    return this->colsSize;
+}
 
-        bool isIndexOutOfBounds(int rowsPosition, int colsPosition) const{
-                if(rowsPosition < 0 || rowsPosition >= this->rowsSize ||
-                   colsPosition < 0 || colsPosition >= this->colsSize){
-                   return true;
-               }
-               return false;
+int Matrix::rows()
+{
+    return this->rowsSize;
+}
+
+void Matrix::print()
+{
+    for (int i = 0; i < matrix.size(); i++) { 
+        for (int j = 0; j < matrix[i].size(); j++) 
+            cout << setprecision(2) << fixed << matrix[i][j] << " "; 
+        cout << endl; 
+    }  
+}
+
+void Matrix::printToFile(ofstream &outputStream)
+{
+    for (int i = 0; i < matrix.size(); i++) { 
+        for (int j = 0; j < matrix[i].size(); j++) 
+            outputStream << matrix[i][j] << " "; 
+        outputStream << endl; 
+    }  
+}
+
+void Matrix::initMatrix(int rowsSize, int colsSize)
+{
+    double initValue = 0.0;
+    for(int indexR = 0; indexR < rowsSize;indexR++) {
+        vector<double> singleRow;
+        for(int indexC = 0; indexC < colsSize ;indexC++) {
+            singleRow.push_back(initValue); // change to 0.0 for prod version
         }
-};
+        matrix.push_back(singleRow);
+    }
+}
+
+void Matrix::readMatrixFromFile(std::ifstream &inputStream){
+    if(!inputStream.is_open()){
+        throw FailureStreamException();
+    }
+    double singlePosition;
+    for (int indexR = 0; indexR < this->rowsSize; indexR++){
+        for (int indexC = 0; indexC < this->colsSize; indexC++){
+            inputStream >> singlePosition;
+            this->set(indexR, indexC, singlePosition);
+        }      
+    }
+}
+
+void Matrix::makeCopyMatrix(Matrix const &matrixToCopy){
+    double singlePosition;
+    for (int indexR = 0; indexR < this->rowsSize; indexR++){
+        for (int indexC = 0; indexC < this->colsSize; indexC++){
+            double value = matrixToCopy.get(indexR, indexC);
+            this->set(indexR, indexC, value);
+        }      
+    }
+}
+
+bool Matrix::isTheSameSize(Matrix m2){
+    if(this->colsSize == m2.colsSize && this->rowsSize == m2.rowsSize){
+        return true;
+    }
+    return false;
+}
+
+bool Matrix::isIndexOutOfBounds(int rowsPosition, int colsPosition) const{
+        if(rowsPosition < 0 || rowsPosition >= this->rowsSize ||
+            colsPosition < 0 || colsPosition >= this->colsSize){
+            return true;
+        }
+        return false;
+}
+
 
 int main() 
 {
+    Matrix matrix(2,3);
+    matrix.set(1,2,3.67);
+    matrix.print();
+    cout<< endl;
     
+    Matrix matrix2(2,3);
+    Matrix matrixExtra(3,2);
+    matrixExtra.set(1,1,3.67);
+    matrixExtra.print();
+    cout << endl;
+    // matrix.add(matrix2).print();
+    Matrix matrix3 = matrix.multiply(matrixExtra);
+    matrix3.print();
+
 }
--- source.h
+++ source.h
@@ -3,20 +3,54 @@
 
 using namespace std;
 
-void printToFile(std::ofstream &outputStream);
+class Matrix 
+{
+    private:
+        int colsSize;
+        int rowsSize;
+        vector<vector<double>> matrix;
 
-void initMatrix(int rowsSize, int colsSize);
+    public:
 
-void set(int rowsPosition, int colsPosition, double val);
+        Matrix();
 
-double get(int rowsPosition, int colsPosition);
+        Matrix(int rowsSize, int colsSize);
 
-int cols();
+        Matrix(int size);
 
-int rows();
+        Matrix(string filename, string path);
 
-void print();
+        Matrix(const Matrix &matrixToCopy);
 
-void readMatrixFromFile(std::ifstream &inputStream);
+        Matrix add(Matrix m2);
 
-bool isIndexOutOfBounds(int rowsPosition, int colsPosition);
+        Matrix subtract(Matrix m2);
+
+        Matrix multiply(Matrix m2);
+
+        void store(string filename, string path);
+
+        void set(int rowsPosition, int colsPosition, double val);
+
+        double get(int rowsPosition, int colsPosition) const;
+
+        int cols();
+
+        int rows();
+
+        void print();
+
+    private:
+        void printToFile(ofstream &outputStream);
+
+        void initMatrix(int rowsSize, int colsSize);
+
+        void readMatrixFromFile(std::ifstream &inputStream);
+
+        void makeCopyMatrix(Matrix const &matrixToCopy);
+
+        bool isTheSameSize(Matrix m2);
+
+        bool isIndexOutOfBounds(int rowsPosition, int colsPosition) const;
+};
+
